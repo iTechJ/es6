a7391997c96e1c597fc25322bd374346e2b01a17
anton.nekrasov <anton.nekrasov@itechart-group.com>
2017-05-02 23:06:43 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        "arrow": "./scripts/arrows.js",
+        "classes": "./scripts/classes.js",
+        "defaults": "./scripts/defaults.js",
+        "destructuring": "./scripts/destructuring.js",
+        "interpolation": "./scripts/interpolation.js",
+        "iterators": "./scripts/iterators.js",
+        "scoping": "./scripts/scoping.js",
+        "maps": "./scripts/maps.js",
+        "module-loaders": "./scripts/module-loaders.js",
+        "modules": "./scripts/modules.js",
+        "literals": "./scripts/object-literals.js",
+        "promises": "./scripts/promises.js"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].build.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
